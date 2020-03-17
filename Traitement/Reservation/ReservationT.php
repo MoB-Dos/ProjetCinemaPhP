24e85515dc66bca564b1316e591769b41208db95
Kikichaton <56584589+Kikichaton@users.noreply.github.com>
2020-03-17 12:04:59 +0100

    il faudra verifier l'hydrate et le constructeur de SetUpReservationlace.php

--- Traitement/Reservation/ReservationT.php
+++ Traitement/Reservation/ReservationT.php
@@ -4,9 +4,15 @@ require '../../Class/ClassManager/reservation/reservationManager.php';
 require '../../Class/SetUp/SetUpReservationPlace.php';
 
 
+$a = new SetUp([
+  'login' => $_POST['login'],
+  'place' => $_POST['place'],
+  'date' => $_POST['date'],
+  'heure' => $_POST['heure'],
+  'film' => $_POST['film'],
+]);
 
-$try = new reservation();
-
-
+$oui = new reservationManager();
+$oui ->reservationM($a);
 
 ?>
